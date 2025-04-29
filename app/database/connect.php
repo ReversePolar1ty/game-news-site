575cea7f7a24a644809628d27e45b5352c04cb5e
Daniil <daniil25072002@gmail.com>
2025-04-30 01:44:33 +0300

    thing

--- app/database/connect.php
+++ app/database/connect.php
@@ -5,7 +5,8 @@ $db_name = 'game-site';
 $db_user = 'root';
 $db_pass = 'mysql';
 $charset = 'utf8';
-$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
 
 try{
     $pdo = new PDO(
---
+++ app/database/db.php
@@ -0,0 +1,82 @@
+﻿<?php
+
+require('connect.php');
+
+function test($value){
+    echo '<pre>';
+    print_r($value);
+    echo '</pre>';
+}
+
+//Проверка выполнения запроса к БД
+function dbCheckError($query){
+    $errInfo = $query->errorInfo();
+    if($errInfo[0] !== PDO::ERR_NONE){
+        echo $errInfo[2];
+        exit();
+    }
+    return true;
+}
+
+//Запрос на получение данных одной таблицы
+function selectAll($table, $params = []){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+
+    if(!empty($params)){
+        $i = 0;
+        foreach ($params as $key => $value) {
+            if($i === 0){
+                $sql = $sql . " WHERE $key = '$value'";
+            } else {
+                $sql = $sql . " AND $key = '$value'";
+            }
+            $i++;
+        }
+    }
+
+
+    $query = $pdo ->prepare($sql);
+    $query -> execute();
+
+    dbCheckError($query);
+
+    return $query->fetchAll();
+
+}
+
+
+//Запрос на получение одних, конкретных данных с выбранной таблицы
+function selectOne($table, $params = []){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+
+    if(!empty($params)){
+        $i = 0;
+        foreach ($params as $key => $value) {
+            if($i === 0){
+                $sql = $sql . " WHERE $key = '$value'";
+            } else {
+                $sql = $sql . " AND $key = '$value'";
+            }
+            $i++;
+        }
+    }
+
+//    $sql = $sql . " LIMIT 1";
+
+    $query = $pdo ->prepare($sql);
+    $query -> execute();
+
+    dbCheckError($query);
+
+    return $query->fetch();
+
+}
+
+$params = [
+    'admin' => 1,
+    'username' => 'gamer1234',
+];
+
+//Запрос на 
